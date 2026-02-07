8d9af1b8907e58a607125ca1c629da89abafabcf
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-02-07 18:41:32 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layout/Navbar"; // Import the component we made
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* We will add a Navbar component here later */}
-        <main className="min-h-screen flex flex-col">
+        {/* The Navbar will now appear at the top of every page */}
+        <Navbar />
+        
+        <main className="min-h-screen flex flex-col bg-slate-950 text-white">
           {children}
         </main>
       </body>
