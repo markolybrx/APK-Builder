82d1f74da67383d11a6b26518cfb1cd26c382b96
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-02-08 15:56:34 +0800

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import Link from 'next/link';
 import { headers } from 'next/headers';
 import { Plus, Search, Terminal, Smartphone, Calendar, Package } from 'lucide-react';
